b349ea2ac442623e7d77d9c211f5f0936cdb2c00
daniel <daniel.aguiar@outlook.fr>
2024-01-24 15:55:26 +0100

    chore: refactor test using the page model pattern

--- playwright/tests/add-employee-should-not-add-employee-when-zipCode-is-negative.spec.ts
+++ playwright/tests/add-employee-should-not-add-employee-when-zipCode-is-negative.spec.ts
@@ -1,23 +1,23 @@
 import { test, expect } from "@playwright/test";
+import { AddEmployeePageModel } from "../pageModels/add-employee-page-model";
 
 test("add employee should not add the employee to the list when zipCode has negative value", async ({
   page,
 }) => {
-  await page.goto("https://q.hr.dmerej.info/add_employee");
-  await page.getByPlaceholder("Name").fill("Daniel");
-  await page.getByPlaceholder("Email").fill("daniel@gmail.com");
-  await page.locator("#id_address_line1").fill("12 rue de la liberté");
-  await page.getByPlaceholder("City").fill("Paris");
-  const zipCode = await page.getByPlaceholder("Zip code");
-  await page.getByPlaceholder("Zip code").fill("-1");
-  await page.getByPlaceholder("Hiring date").fill("2024-01-16");
-  await page.getByPlaceholder("Job title").fill("Developer");
-  await page.getByRole("button", { name: "Add" }).click();
-  expect(page.url()).toBe("https://q.hr.dmerej.info/add_employee");
+  const addEmployeePage = new AddEmployeePageModel(page);
 
-  const zipCodeValidity = await zipCode.evaluate((element) => {
-    const input = element as HTMLInputElement;
-    return input.validity.valid;
-  });
-  expect(zipCodeValidity).toBe(false);
+  await addEmployeePage.fillForm(
+    "Daniel",
+    "daniel@gmail.com",
+    "12 rue de la liberté",
+    "Paris",
+    "-1",
+    "2024-01-16",
+    "Developer"
+  );
+  const zipCode = addEmployeePage.getByName("zip_code");
+  await addEmployeePage.clickOnAddButton();
+
+  expect(addEmployeePage.isTheSamePage).toBe(true);
+  expect(addEmployeePage.isInputValid(zipCode)).toBe(false);
 });
